94afafef534c1d0dd849e4f6ba19823122618303
Sergio Kopplin <sergiokopplin@gmail.com>
2021-09-24 23:07:02 -0300

    feat: connects account repository into db add account

--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -1,9 +1,11 @@
 import { AccountModel, AddAccount } from '@/domain';
+import { AccountRepository } from '@/data';
 
 export class DbAddAccount implements AddAccount {
-  constructor() {}
+  constructor(private readonly accountRepository: AccountRepository) {}
 
   async add(account: AddAccount.AddAccountParams): Promise<AccountModel> {
-    return null;
+    const result = await this.accountRepository.add(account);
+    return result;
   }
 }
